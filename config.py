442a4e5b731c6f1954a21685e5094c17d8cec873
FerLaiVer <fernando.laibe@gmail.com>
2026-04-17 08:10:33 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+INPUT_DIR = BASE_DIR / "temp" / "input"
+OUTPUT_DIR = BASE_DIR / "temp" / "output"
+
+OPEN3SDCM_BIN = "/usr/local/bin/open3sdcm"  # ruta del conversor en el servidor
